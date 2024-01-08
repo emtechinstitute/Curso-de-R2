9f538c04cf3295103ce4ca900f340592228fddd8
Emerging Technologies Institute <69870228+emtechinstitute@users.noreply.github.com>
2024-01-08 09:10:41 -0600

    Update test-tu_script.R

--- tests/testthat/test-tu_script.R
+++ tests/testthat/test-tu_script.R
@@ -3,11 +3,20 @@
 library(testthat)
 #library(tu_paquete) # si tu código está en un paquete
 
-context("Pruebas para tu_script.R")
+#context("Pruebas para tu_script.R")
 
-test_that("Función suma correctamente", {
-  result <- tu_funcion_de_suma(1, 1)
-  expect_equal(result, 2)
-})
+#test_that("Función suma correctamente", {
+  #result <- tu_funcion_de_suma(1, 1)
+  #expect_equal(result, 2)
+#})
 
 # Puedes añadir más tests para diferentes funciones o casos de uso
+
+# instalar el paquete dslabs
+install.packages("dslabs")
+
+# cargar el paquete dslabs en la sesión de R
+library(dslabs)
+
+#calcular propina
+0.15 * 19.71  
